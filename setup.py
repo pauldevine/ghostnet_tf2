fbe9c7be80522a628ef4656111edb3538d3e3d05
Paul Devine <pauldevine@users.noreply.github.com>
2020-07-18 17:54:15 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ghostnet_tf2',
+      version='0.1',
+      description='An implementation of GhostNet for Tensorflow 2.0+ (From the paper "GhostNet: More Features from Cheap Operations")',
+      url='https://github.com/CarloLepelaars/ghostnet_tf2',
+      author='Carlo Lepelaars',
+      author_email='info@carlolepelaars.nl',
+      license='MIT',
+      packages=['ghostnet_tf2'],
+      zip_safe=False)
